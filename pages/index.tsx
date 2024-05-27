55ecd1056e94ac4cf039cfbf3cd0c9bba3c4fa03
신민철 <homerjay@naver.com>
2024-05-28 00:23:07 +0900

    add: register, login screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-    <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+    <h1 className="text-2xl text-green-500">NetflixClsdafsdsdafsdfdsfasfadslfjVoluptate laboris id exercitation nisi culpa Lorem amet cupidatat est ipsum. Quis cupidatat consequat nisi est aute sit nulla nostrud minim esse. Voluptate esse ipsum ut nulla dolor commodo excepteur ex sit qui consequat duis ex.adsklfjdsljflkdasjflkjdsljflkdsjflkasjdlfkjdslkfjone</h1>
     </>
   );
 }
